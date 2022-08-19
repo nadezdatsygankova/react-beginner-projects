fb0278bc77cefe74fc9fb50afe33498ceb4e56a7
nadezdatsygankova <nadiapritchina@gamil.com>
2022-08-19 09:22:00 -0400

    feat: add logic in quiz

--- src/App.js
+++ src/App.js
@@ -1,58 +1,75 @@
 import './index.scss';
+import React from 'react';
 
 const questions = [
   {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
+    title: 'What is React?',
+    variants: ['Library', 'Framework', 'App'],
     correct: 0,
   },
   {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    title: 'What is a component? ',
+    variants: ['App', 'Part of an application or page', 'Not sure'],
     correct: 1,
   },
   {
-    title: 'Что такое JSX?',
+    title: 'What is JSX?',
     variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
+      'This is an HTML',
+      'This is a function',
+      'This is HTML, with JS-code inside',
     ],
     correct: 2,
   },
 ];
 
-function Result() {
+function Result({correct}) {
   return (
     <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <img alt = "party" src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>You guessed {correct} answers out of {questions.length}</h2>
+      <a href ='/' >
+      <button>Try again</button>
+      </a>
     </div>
   );
 }
 
-function Game() {
+function Game({question, onClickVariant, step}) {
+  const percentage = Math.round(step / questions.length * 100)
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {
+          question.variants.map((text, index) => (
+          <li key = {text} onClick = {() => onClickVariant(index)}>{text}</li>))
+        }
       </ul>
     </>
   );
 }
 
 function App() {
+  const [step, setStep] = React.useState(0);
+  const [correct, setCorrect] = React.useState(0);
+  const question = questions[step];
+  const onClickVariant = (index) => {
+     console.log(step, index);
+     setStep(step + 1);
+     if(index === question.correct){
+      setCorrect(correct + 1)
+     }
+  }
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {
+        step !== questions.length ? <Game step={step} question = {question} onClickVariant = {onClickVariant}/>:
+        <Result correct = {correct} />  }
+
     </div>
   );
 }
